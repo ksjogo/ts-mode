dd66583c155cdfd1ac64d0b8fa869b4c6f3ed408
ksjogo <jogo@kronberger-spiele.de>
2016-06-16 17:53:00 +0200

    typoscripted

--- typoscript-mode.el
+++ typoscript-mode.el
@@ -1,4 +1,4 @@
-;;; ts-mode.el --- mode for TypoScript files
+;;; typoscript-mode.el --- mode for TypoScript files
 
 ;; Copyright (C) 2009  Joachim Mathes
 ;; Copyright (C) 2016 Johannes Goslar
@@ -31,25 +31,25 @@
 
 ;;; Code:
 
-(defconst ts-version "0.1"
-  "`ts-mode' version number.")
+(defconst typoscript-version "0.1"
+  "`typoscript-mode' version number.")
 
 ;; User definable variables
 ;; vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
 
 (defgroup typoscript nil
   "Major mode for editing TypoScript files."
-  :prefix "ts-"
+  :prefix "typoscript-"
   :group 'languages)
 
-(defcustom ts-newline-function 'newline-and-indent
+(defcustom typoscript-newline-function 'newline-and-indent
   "Function to be called upon pressing `RET'."
   :type '(choice (const newline)
                  (const newline-and-indent)
                  (const reindent-then-newline-and-indent))
   :group 'typoscript)
 
-(defcustom ts-block-indentation 2
+(defcustom typoscript-block-indentation 2
   "The indentation relative to a predecessing line which begins a new block.
 
   In TypoScript blocks start with the left parenthesis `(' or the left brace
@@ -57,7 +57,7 @@
   :type 'integer
   :group 'typoscript)
 
-(defcustom ts-fold-foreground-color "white"
+(defcustom typoscript-fold-foreground-color "white"
   "The foreground color used to highlight the folded block.
 
   The default value is `white'.  For a list of all available colors use `M-x
@@ -65,7 +65,7 @@ list-colors-display'"
   :type 'color
   :group 'typoscript)
 
-(defcustom ts-fold-background-color "DodgerBlue1"
+(defcustom typoscript-fold-background-color "DodgerBlue1"
   "The background color used to highlight the folded block.
 
   The default value is `DodgerBlue1'.  For a list of all available colors use
@@ -73,42 +73,42 @@ list-colors-display'"
   :type 'color
   :group 'typoscript)
 
-(defface ts-classes-face
+(defface typoscript-classes-face
   '((t :inherit font-lock-keyword-face))
   "Face for TypoScript classes.")
 
-(defface ts-path-face
+(defface typoscript-path-face
   '((t :inherit font-lock-builtin-face :foreground "DarkTurquoise"))
   "Face for TypoScript paths.")
 
-(defface ts-block-face
+(defface typoscript-block-face
   '((t :inherit font-lock-builtin-face :foreground "DodgerBlue1"))
   "Face for TypoScript blocks.")
 
-(defface ts-conditional-face
+(defface typoscript-conditional-face
   '((t :inherit font-lock-keyword-face))
   "Face for TypoScript conditionals.")
 
-(defface ts-html-face
+(defface typoscript-html-face
   '((t :inherit font-lock-string-face))
   "Face for TypoScript HTML tags.")
 
-(defvar ts-classes-face 'ts-classes-face
+(defvar typoscript-classes-face 'typoscript-classes-face
   "Face for TypoScript classes.")
 
-(defvar ts-path-face 'ts-path-face
+(defvar typoscript-path-face 'typoscript-path-face
   "Face for TypoScript paths.")
 
-(defvar ts-block-face 'ts-block-face
+(defvar typoscript-block-face 'typoscript-block-face
   "Face for TypoScript blocks.")
 
-(defvar ts-conditional-face 'ts-conditional-face
+(defvar typoscript-conditional-face 'typoscript-conditional-face
   "Face for TypoScript conditionals.")
 
-(defvar ts-html-face 'ts-html-face
+(defvar typoscript-html-face 'typoscript-html-face
   "Face for TypoScript HTML tags.")
 
-(defvar ts-font-lock-keywords
+(defvar typoscript-font-lock-keywords
   (let ((kw1 (mapconcat 'identity
                         ;; Basic TypoScript classes
                         '("CONFIG"   "PAGE"  "TEXT"       "COA"  "COA_INT"
@@ -117,85 +117,85 @@ list-colors-display'"
                         "\\|")))
     (list
      ;; Paths
-     '("^[ \t]*\\([[:alnum:]-_\\.]+\\)[ \t]*[=<>]" 1 'ts-path-face)
+     '("^[ \t]*\\([[:alnum:]-_\\.]+\\)[ \t]*[=<>]" 1 'typoscript-path-face)
      ;; Blocks
-     '("^[ \t]*\\([[:alnum:]-_\\.]+\\)[ \t]*[{(]" 1 'ts-block-face)
+     '("^[ \t]*\\([[:alnum:]-_\\.]+\\)[ \t]*[{(]" 1 'typoscript-block-face)
      ;; Periods
      ;;'("^[ \t]*" "\\(\\.\\)" nil nil (1 'default t))
      ;; Classes (keywords)
-     (list (concat "\\<\\(" kw1 "\\)\\>") 1 'ts-classes-face t)
+     (list (concat "\\<\\(" kw1 "\\)\\>") 1 'typoscript-classes-face t)
      ;; Conditional expressions `[...]'
-     '("^[ \t]*\\(\\[.+?\\]\\)[ \t]*$" 1 'ts-conditional-face)
+     '("^[ \t]*\\(\\[.+?\\]\\)[ \t]*$" 1 'typoscript-conditional-face)
      ;; Comment lines beginning with hash symbol `#'
      '("^[ \t]*\\(#.*\\)$" 1 'font-lock-comment-face)
      ;; HTML special character encodings on the right side of the operator
-     '("\\(=\\|=<\\|>\\|:=\\)" "\\(&[#[:alnum:]]+;\\)" nil nil (0 'ts-html-face))
+     '("\\(=\\|=<\\|>\\|:=\\)" "\\(&[#[:alnum:]]+;\\)" nil nil (0 'typoscript-html-face))
      ;; HTML tags
-     '("=<?\\|>\\|:=\\|[ \t]*" "\\(<[^<]+?>\\)" nil nil (0 'ts-html-face))
+     '("=<?\\|>\\|:=\\|[ \t]*" "\\(<[^<]+?>\\)" nil nil (0 'typoscript-html-face))
      ;; HTML color definitions
-     '("#[[:xdigit:]]\\{6\\}[ \t\n]+" 0 'ts-html-face t)))
+     '("#[[:xdigit:]]\\{6\\}[ \t\n]+" 0 'typoscript-html-face t)))
   "Expressions to highlight in TypoScript mode.")
 
-(defvar ts-highlight-overlays [nil nil]
+(defvar typoscript-highlight-overlays [nil nil]
   "A vector of different overlay to do highlighting.
 This vector concerns only highlighting of horizontal lines.")
 
-(defvar ts-mode-map
+(defvar typoscript-mode-map
   (let ((map (make-sparse-keymap)))
-    (define-key map "\r" 'ts-newline)
-    (define-key map "\C-c\C-e" 'ts-fold-block)
-    (define-key map "\C-c\C-a" 'ts-unfold-block)
-    (define-key map "\C-c\C-u\C-r" 'ts-unfold-region)
-    (define-key map "\C-c\C-u\C-b" 'ts-unfold-buffer)
-    (define-key map "}" 'ts-electric-brace)
-    (define-key map ")" 'ts-electric-brace)
+    (define-key map "\r" 'typoscript-newline)
+    (define-key map "\C-c\C-e" 'typoscript-fold-block)
+    (define-key map "\C-c\C-a" 'typoscript-unfold-block)
+    (define-key map "\C-c\C-u\C-r" 'typoscript-unfold-region)
+    (define-key map "\C-c\C-u\C-b" 'typoscript-unfold-buffer)
+    (define-key map "}" 'typoscript-electric-brace)
+    (define-key map ")" 'typoscript-electric-brace)
     map)
   "Key map used in TypoScript Mode buffers.")
 
-(defvar ts-mode-syntax-table
-  (let ((ts-mode-syntax-table (make-syntax-table)))
+(defvar typoscript-mode-syntax-table
+  (let ((typoscript-mode-syntax-table (make-syntax-table)))
     ;; Parenthesis, brackets and braces
-    (modify-syntax-entry ?\( "()" ts-mode-syntax-table)
-    (modify-syntax-entry ?\) ")(" ts-mode-syntax-table)
-    (modify-syntax-entry ?\[ "(]" ts-mode-syntax-table)
-    (modify-syntax-entry ?\] ")[" ts-mode-syntax-table)
-    (modify-syntax-entry ?\{ "(}" ts-mode-syntax-table)
-    (modify-syntax-entry ?\} "){" ts-mode-syntax-table)
+    (modify-syntax-entry ?\( "()" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\) ")(" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\[ "(]" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\] ")[" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\{ "(}" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\} "){" typoscript-mode-syntax-table)
     ;; Comment delimiters
-    (modify-syntax-entry ?/ ". 124b" ts-mode-syntax-table)
-    (modify-syntax-entry ?* ". 23" ts-mode-syntax-table)
-    (modify-syntax-entry ?\n "> b" ts-mode-syntax-table)
-    (modify-syntax-entry ?\" "." ts-mode-syntax-table)
-    (modify-syntax-entry ?. "." ts-mode-syntax-table)
-    ts-mode-syntax-table)
+    (modify-syntax-entry ?/ ". 124b" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?* ". 23" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\n "> b" typoscript-mode-syntax-table)
+    (modify-syntax-entry ?\" "." typoscript-mode-syntax-table)
+    (modify-syntax-entry ?. "." typoscript-mode-syntax-table)
+    typoscript-mode-syntax-table)
   "Syntax table used in TypoScript Mode buffers.")
 
-(defcustom ts-mode-hook nil
+(defcustom typoscript-mode-hook nil
   "Hook run when entering TypoScript mode."
   :options '()
   :type 'hook
   :group 'ts)
 
 ;;;###autoload
-(define-derived-mode ts-mode fundamental-mode "TypoScript"
+(define-derived-mode typoscript-mode fundamental-mode "TypoScript"
   "Major mode for editing TypoScript files."
   :group 'ts
-  (set (make-local-variable 'font-lock-defaults) '(ts-font-lock-keywords))
+  (set (make-local-variable 'font-lock-defaults) '(typoscript-font-lock-keywords))
   (set (make-local-variable 'comment-start) "# ")
   (set (make-local-variable 'comment-end) "")
   (set (make-local-variable 'comment-start-skip) "# ")
-  (set (make-local-variable 'indent-line-function) 'ts-indent-line)
+  (set (make-local-variable 'indent-line-function) 'typoscript-indent-line)
   (set (make-local-variable 'defun-prompt-regexp) "^[ \t]*\\([[:alnum:]-_\\.]+\\)[ \t]*"))
 
-(defun ts-newline ()
+(defun typoscript-newline ()
   "Call the dedicated newline function.
 
-The variable `ts-newline-function' decides which newline function to
+The variable `typoscript-newline-function' decides which newline function to
 use."
   (interactive)
-  (funcall ts-newline-function))
+  (funcall typoscript-newline-function))
 
-(defun ts-indent-line ()
+(defun typoscript-indent-line ()
   "Indent current line for TypoScript mode."
   (let ((cp (point))                ; current point
         (cc (current-column))       ; current column
@@ -209,7 +209,7 @@ use."
     ;; Evaluate parser state
     (save-excursion
       (beginning-of-line)
-      (setq ps (ts-parser-state))
+      (setq ps (typoscript-parser-state))
 
       (cond
        ;; Check if parser state position is:
@@ -233,7 +233,7 @@ use."
           (goto-char psp)
           (back-to-indentation)
           (setq save-indent-column (+ (current-column)
-                                      ts-block-indentation)))))
+                                      typoscript-block-indentation)))))
        ;; Check if parser state position is:
        ;; -> nil
        (t
@@ -252,7 +252,7 @@ use."
     (if (> cc ci)
         (forward-char (- cc ci)))))
 
-(defun ts-parser-state ()
+(defun typoscript-parser-state ()
   "Return the parser state at point."
   (save-excursion
     (let ((here (point))
@@ -265,18 +265,18 @@ use."
         (setq sps (parse-partial-sexp (point) here)))
       sps)))
 
-(defun ts-block-start ()
+(defun typoscript-block-start ()
   "Return buffer position of the last unclosed enclosing block.
 
 If nesting level is zero, return nil."
-  (let ((status (ts-parser-state)))
+  (let ((status (typoscript-parser-state)))
     (if (<= (car status) 0)
         nil
       (car (cdr status)))))
 
 ;; Electric characters
 
-(defun ts-electric-brace (arg)
+(defun typoscript-electric-brace (arg)
   "Insert closing brace.
 Argument ARG prefix."
   (interactive "*P")
@@ -285,22 +285,22 @@ Argument ARG prefix."
 
   (when (and (looking-at "[ \t]*$")
              (looking-back "^[ \t]*[})]"))
-    (ts-indent-line)))
+    (typoscript-indent-line)))
 
 ;; Folding
 
-(defun ts-fold-block ()
+(defun typoscript-fold-block ()
   "Hide the block on which point currently is located."
   (interactive)
   (let ((current-point (point))
-        (block-start (ts-block-start)))
+        (block-start (typoscript-block-start)))
 
     (if (not block-start)
         (message "Point is not within a block.")
 
       ;; Look for block start
       (save-excursion
-        (goto-char (ts-block-start))
+        (goto-char (typoscript-block-start))
         (beginning-of-line)
         (setq block-start (point)))
 
@@ -334,71 +334,71 @@ Argument ARG prefix."
           ;; Check if end of measurement block is beyond point;
           ;; call fold function otherwise
           (if (>= block-end current-point)
-              (ts-fold block-start block-end block-name)
+              (typoscript-fold block-start block-end block-name)
             (message "Error: No valid block found."))
 
           ;; Indent overlay
           (goto-char block-start)
           (beginning-of-line)
-          (ts-indent-line))))))
+          (typoscript-indent-line))))))
 
-(defun ts-fold (block-start block-end block-name)
+(defun typoscript-fold (block-start block-end block-name)
   "Fold block.
 
 The block starts at BLOCK-START and ends at BLOCK-END.  Its
 BLOCK-NAME is the TypoScript object path."
-  (let (ts-overlay)
+  (let (typoscript-overlay)
     ;; Check if block-start and block-end are valid values, i.e. not nil
     (if (or (eq block-start nil)
             (eq block-end nil))
         (message "Error: No valid block found.")
       ;; Make an overlay and hide block
-      (setq ts-overlay (make-overlay block-start block-end
+      (setq typoscript-overlay (make-overlay block-start block-end
                                      (current-buffer) t nil))
-      (overlay-put ts-overlay 'category 'ts-fold)
-      (overlay-put ts-overlay 'evaporate t)
-      (overlay-put ts-overlay 'mouse-face 'highlight)
-      (overlay-put ts-overlay 'display (concat "["
+      (overlay-put typoscript-overlay 'category 'typoscript-fold)
+      (overlay-put typoscript-overlay 'evaporate t)
+      (overlay-put typoscript-overlay 'mouse-face 'highlight)
+      (overlay-put typoscript-overlay 'display (concat "["
                                                (propertize block-name
                                                            'face
                                                            nil)
                                                "]"))
-      (overlay-put ts-overlay 'font-lock-face `(:foreground ,ts-fold-foreground-color
-                                                            :background ,ts-fold-background-color))
-      (overlay-put ts-overlay 'help-echo (concat
+      (overlay-put typoscript-overlay 'font-lock-face `(:foreground ,typoscript-fold-foreground-color
+                                                            :background ,typoscript-fold-background-color))
+      (overlay-put typoscript-overlay 'help-echo (concat
                                           "Folded block: "
                                           block-name)))))
 
-(defun ts-unfold-buffer ()
+(defun typoscript-unfold-buffer ()
   "Unfold all blocks in the buffer."
   (interactive)
-  (ts-unfold-region (point-min) (point-max)))
+  (typoscript-unfold-region (point-min) (point-max)))
 
-(defun ts-unfold-region (start end)
+(defun typoscript-unfold-region (start end)
   "Unfold all blocks in the region.
 
 The region delimiters are START and END."
   (interactive "r")
-  (let ((ts-overlays (overlays-in start end)))
-    (ts-unfold-overlays ts-overlays)))
+  (let ((typoscript-overlays (overlays-in start end)))
+    (typoscript-unfold-overlays typoscript-overlays)))
 
-(defun ts-unfold-block ()
+(defun typoscript-unfold-block ()
   "Unfold block at point."
   (interactive)
-  (let ((ts-overlays (overlays-at (point))))
-    (ts-unfold-overlays ts-overlays)))
+  (let ((typoscript-overlays (overlays-at (point))))
+    (typoscript-unfold-overlays typoscript-overlays)))
 
-(defun ts-unfold-overlays (ts-overlays)
-  "Unfold all overlays set by ts-fold in TS-OVERLAYS.
+(defun typoscript-unfold-overlays (typoscript-overlays)
+  "Unfold all overlays set by typoscript-fold in TYPOSCRIPT-OVERLAYS.
 
 Return non-nil if an unfold happened, nil otherwise."
   (let (found)
-    (dolist (overlay ts-overlays)
-      (when (eq (overlay-get overlay 'category) 'ts-fold)
+    (dolist (overlay typoscript-overlays)
+      (when (eq (overlay-get overlay 'category) 'typoscript-fold)
         (delete-overlay overlay)
         (setq found t)))
     found))
 
-(provide 'ts-mode)
+(provide 'typoscript-mode)
 
-;;; ts-mode.el ends here
+;;; typoscript-mode.el ends here
